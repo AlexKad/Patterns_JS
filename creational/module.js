6f81c3f7c14f73b4c18ce4cbabd3e7c8660c45f8
Alex Kad <Alexandra_Kadushkina@epam.com>
2016-03-29 18:42:21 +0300

    module pattern

---
+++ creational/module.js
@@ -0,0 +1,17 @@
+
+//module pattern 
+// 'Toolbox' of functions
+
+var Module = function() {
+
+	return {
+		method: function(){},
+
+		nextMethod:function(){}
+	}	
+};
+
+
+var toolbox = Module();
+
+toolbox.method();
